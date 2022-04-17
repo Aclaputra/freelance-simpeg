76ee574c4d1d22ad1036863925dc2963318cfe72
aclaputra <muhamadacla22@gmail.com>
2022-04-17 22:29:37 +0700

    added APP Layout

---
+++ app/View/Components/AtasanPejabatPenilaiLayout.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class AtasanPejabatPenilaiLayout extends Component
+{
+    /**
+     * Get the view / contents that represents the component.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function render()
+    {
+        return view('atasan_pejabat_penilai.layouts.app');
+    }
+}
