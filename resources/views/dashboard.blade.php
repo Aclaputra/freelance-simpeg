1107fc3c8cde01069ed9eb60ef4b42fe312fa574
Aclaputra <muhamadacla22@gmail.com>
2022-05-16 02:15:12 +0700

    added dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,40 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }} {{ Auth::guard('web')->user()->name }} - ({{ Auth::guard('web')->user()->email }})
+            {{ __('Dashboard') }} Pegawai
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    You're logged in as {{ Auth::guard('web')->user()->name }} with Email: {{ Auth::guard('web')->user()->email }}
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="py-2">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    Dashboard
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    Tables
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+  <div class="shrink-0">
+    <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
+  </div>
+  <div>
+    <div class="text-xl font-medium text-black">ChitChat</div>
+    <p class="text-slate-500">You have a new message!</p>
+  </div>
+</div> -->
 </x-app-layout>
