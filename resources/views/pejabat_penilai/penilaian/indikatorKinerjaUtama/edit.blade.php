b2fd48575e1aff8cfa0eeacd99f0caa340c7dde8
Aclaputra <muhamadacla22@gmail.com>
2022-07-13 10:39:05 +0700

    feat: modifeid iku edit

--- resources/views/pejabat_penilai/penilaian/indikatorKinerjaUtama/edit.blade.php
+++ resources/views/pejabat_penilai/penilaian/indikatorKinerjaUtama/edit.blade.php
@@ -35,7 +35,7 @@
                   <span class="count bg-success"></span>
                 </div>
                 <div class="profile-name">
-                  <h5 class="mb-0 font-weight-normal">Henry Klein</h5>
+                  <h5 class="mb-0 font-weight-normal">{{ Auth::guard('pejabat_penilai')->user()->name }}</h5>
                   <span>Gold Member</span>
                 </div>
               </div>
@@ -264,7 +264,7 @@
                 <a class="nav-link" id="profileDropdown" href="#" data-toggle="dropdown">
                   <div class="navbar-profile">
                     <img class="img-xs rounded-circle" src="{{ asset('../../assets/images/faces/face15.jpg') }}" alt="">
-                    <p class="mb-0 d-none d-sm-block navbar-profile-name">Henry Klein</p>
+                    <p class="mb-0 d-none d-sm-block navbar-profile-name">{{ Auth::guard('pejabat_penilai')->user()->name }}</p>
                     <i class="mdi mdi-menu-down d-none d-sm-block"></i>
                   </div>
                 </a>
