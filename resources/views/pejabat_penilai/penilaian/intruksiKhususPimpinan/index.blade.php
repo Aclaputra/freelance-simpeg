679bafdec544ae09e42efe1ad5df2a732a837138
Aclaputra <muhamadacla22@gmail.com>
2022-07-14 13:13:03 +0700

    feat: modified resources/views/pejabat_penilai/penilaian/intruksiKhususPimpinan/index.blade.php

--- resources/views/pejabat_penilai/penilaian/intruksiKhususPimpinan/index.blade.php
+++ resources/views/pejabat_penilai/penilaian/intruksiKhususPimpinan/index.blade.php
@@ -390,7 +390,7 @@
                               {{ $data->nip }}
                             </td>
                             <td> {{ $data->ikp }} </td>
-                            <td> {{ $data->penilaian }}</td>
+                            <td> {{ $data->penilaian_ikp }}</td>
                             <td>
                               <a href="{{ url('pejabat_penilai/dashboard/komponen_penilaian_pekerjaan/ikp/' . $data->id . '/edit') }}">
                                 <button type="button" class="btn btn-outline-primary">Ubah</button>
