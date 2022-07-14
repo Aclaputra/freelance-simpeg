24c33a52010f1dd480a8c6371a36f3f45933a674
Aclaputra <muhamadacla22@gmail.com>
2022-07-14 13:13:43 +0700

    feat: modifeid resources/views/pejabat_penilai/penilaian/perilaku/index.blade.php'

--- resources/views/pejabat_penilai/penilaian/perilaku/index.blade.php
+++ resources/views/pejabat_penilai/penilaian/perilaku/index.blade.php
@@ -390,7 +390,7 @@
                               {{ $data->nip }}
                             </td>
                             <td> {{ $data->perilaku }} </td>
-                            <td> {{ $data->penilaian }}</td>
+                            <td> {{ $data->penilaian_perilaku }}</td>
                             <td>
                               <a href="{{ url('pejabat_penilai/dashboard/komponen_penilaian_pekerjaan/perilaku/' . $data->id . '/edit') }}">
                                 <button type="button" class="btn btn-outline-primary">Ubah</button>
